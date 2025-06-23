5c4339ec3b34c30f6fdc81a66072e9ef07c0e609
Fabiana emanuela <fabymanuandremarcolino@gmail.com>
2025-06-23 17:56:10 -0300

    Integrar base do projeto crud de users

---
+++ resources/views/livewire/users/index.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <x-card>
+
+
+        <div class="mb-2 mt-4">
+            <livewire:users.create @created="$refresh" />
+        </div>
+
+        <x-table :$headers :$sort :rows="$this->rows" paginate simple-pagination filter loading :quantity="[2, 5, 15, 25]">
+            @interact('column_created_at', $row)
+            {{ $row->created_at->diffForHumans() }}
+            @endinteract
+
+            @interact('column_action', $row)
+            <div class="flex gap-1">
+                <x-button.circle icon="pencil" wire:click="$dispatch('load::user', { 'user' : '{{ $row->id }}'})" />
+                <livewire:users.delete :user="$row" :key="uniqid('', true)" @deleted="$refresh" />
+            </div>
+            @endinteract
+        </x-table>
+    </x-card>
+
+    <livewire:users.update @updated="$refresh" />
+</div>
